de96fbfdab3f83c4012b85159411299fc86666a0
Matt Heath <matt@mattheath.com>
2014-09-21 21:32:21 +0100

    Add initial fetch methods

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	html "code.google.com/p/go.net/html"
+	atom "code.google.com/p/go.net/html/atom"
+	"github.com/PuerkitoBio/goquery"
+	log "github.com/cihub/seelog"
+)
+
+func main() {
+	// Crawl the specified site
+	Crawl("http://golang.org/")
+}
+
+// Crawl takes a URL and recursively crawls pages
+func Crawl(url string) {
+
+	_, urls, err := fetch(url)
+	if err != nil {
+		log.Errorf("Error:", err)
+		return
+	}
+
+	log.Infof("URLs found: %+v", urls)
+}
+
+// fetch retrieves the page at the specified URL and extracts URLs
+func fetch(url string) (string, []string, error) {
+
+	urls := make([]string, 0)
+
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return "", nil, err
+	}
+
+	sel := doc.Find("a")
+	for i, n := range sel.Nodes {
+		if n.Type != html.ElementNode || n.DataAtom != atom.A {
+			log.Debugf("Node is not an anchor: %v", n.Type)
+			continue
+		}
+
+		var href string
+
+		for _, a := range n.Attr {
+			if a.Key != "href" {
+				continue
+			}
+			href = a.Val
+		}
+
+		if href == "" {
+			continue
+		}
+
+		log.Infof("Node %v: %s", i, href)
+		urls = append(urls, href)
+	}
+
+	log.Debugf("URLs: %+v", urls)
+
+	return "body", urls, nil
+}
